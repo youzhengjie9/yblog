1cd08f3575ab82f7737e29a67611594420b9dbc3
ms666 <1550324080@qq.com>
2021-05-28 22:26:09 +0800

    2021/5/28

--- src/main/java/com/boot/controller/userController.java
+++ src/main/java/com/boot/controller/userController.java
@@ -1,21 +1,73 @@
 package com.boot.controller;
 
+
+import com.alibaba.fastjson.JSON;
+import com.boot.data.ResponseData.ArticleResponseData;
 import com.boot.utils.Commons;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Controller
 @RequestMapping(path = "/user")
 public class userController {
 
-    @RequestMapping(path = "/")
-    public String toUserList(Model model){
+    @RequestMapping(path = "/list")
+//    @ResponseBody
+    public String toUserList(@RequestParam(value = "option",defaultValue = "1") int option, Model model){
+        //通过option来判断显示基本资料还是密码修改
+        //1就是基本资料  2就是密码修改
+        //为了防止用户修改传入的option值，我们就采取option==2，密码修改，option!=2，基本资料
+
+//        System.out.println(option);
+//        if(option==2){
+//            model.addAttribute("op",2);
+//        }else {
+//            model.addAttribute("op",1);
+//        }
+
 
         model.addAttribute("commons", Commons.getInstance());
 
+
+
         return "back/user_list";
     }
+
+
+    @RequestMapping(path = "/upload")
+//    @ResponseBody
+    public String upload(Model model,MultipartFile file){
+        System.out.println("============");
+        System.out.println(file.getOriginalFilename());
+        System.out.println(file.getName());
+        model.addAttribute("commons",Commons.getInstance());
+
+        return "back/user_list";
+
+    }
+
+
+
+
+
+
+
 
 }
