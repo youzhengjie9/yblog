92f12fa5e85428b93d8b28489b279cd5e64fa1b8
ms666 <1550324080@qq.com>
2021-06-27 16:24:13 +0800

    2021/6/27 新后台系统数据图表适配完成

--- src/main/java/com/boot/controller/pearAdmin/pearController.java
+++ src/main/java/com/boot/controller/pearAdmin/pearController.java
@@ -575,6 +575,24 @@ public class pearController {
         return JSON.toJSONString(layuiJSON);
     }
 
+    @ResponseBody
+    @RequestMapping(path = "/updatePassword")
+    public String updatePassword(password password){
+
+        System.out.println(password);
+
+        layuiJSON layuiJSON = new layuiJSON();
+        layuiJSON.setMsg("666");
+        if(password.getNewPassword().equals(password.getCheckPassword())){
+            layuiJSON.setSuccess(true);
+        }else {
+            layuiJSON.setSuccess(false);
+        }
+        return JSON.toJSONString(layuiJSON);
+    }
+
+
+
 
     //访客记录
     @Visitor(desc = "访客记录")
@@ -671,7 +689,65 @@ public class pearController {
 
     //数据图表
     @RequestMapping(path = "/toEcharts")
-    public String toEcharts() {
+    public String toEcharts(Model model,HttpSession session) {
+
+        //提供文章阅读量数据
+        List<Article> articles = articleService.selectArticleStatistic();
+        model.addAttribute("articles", articles);
+
+        //提供分类数的数据
+        List<category> categories = categoryService.selectCategories_echarts();
+        model.addAttribute("categories", categories);
+        //提供标签数的数据
+        List<tag> tags = tagService.selectTags_echarts();
+//        tags.get(0).getTagName()
+//        tags.get(0).getTagCount()
+        model.addAttribute("tags", tags);
+
+        //从缓存中查有没有近7天的缓存
+
+        Object var1 =redisTemplate.opsForValue().get(ECHARTS_DAYS);
+        Object var2 =redisTemplate.opsForValue().get(ECHARTS_COUNTS);
+
+        List<String> ds = JSON.parseArray((String) var1, String.class);
+        List<Integer> cs = JSON.parseArray((String) var2, Integer.class);
+
+        if (ds == null || ds.size() < 7 || cs == null || cs.size() < 7) { //这种情况就要重新查
+            List<String> days = visitorService.selectDaysBy7(); //维护日期
+            List<Integer> counts = new ArrayList<>(); //维护访问量
+            for (String day : days) {
+                int count = visitorService.selectOneDayVisitor(day);
+                counts.add(count);
+            }
+            model.addAttribute("days", days);
+            model.addAttribute("counts", counts);
+            //让集合变成json放入redis
+            String d = JSON.toJSONString(days);
+            String c = JSON.toJSONString(counts);
+            //让缓存在晚上12点整点就失效
+            Long second = timeUtil.getSecondByCurTimeTo12Point();
+            redisTemplate.opsForValue().set(ECHARTS_DAYS, d, second, TimeUnit.SECONDS);
+            redisTemplate.opsForValue().set(ECHARTS_COUNTS, c, second, TimeUnit.SECONDS);
+        } else {
+            //如果还有缓存（说明没有过一天），并且数据没被篡改过（都是7个数据），就执行这里的代码
+            //这个时候我们只需要更新一下最后一天（也就是今天）的数据即可
+            String s = ds.get(6); //获取今天的日期
+            int i = visitorService.selectOneDayVisitor(s);
+            cs.set(6,i);
+            String list = JSON.toJSONString(cs); ////记得转换成json
+            model.addAttribute("days", ds);
+            model.addAttribute("counts", cs);
+            //重新放入redis
+            Long second = timeUtil.getSecondByCurTimeTo12Point();
+            redisTemplate.opsForValue().set(ECHARTS_COUNTS, list, second, TimeUnit.SECONDS);
+
+        }
+
+        String username = springSecurityUtil.currentUser(session);
+        userDetail userDetail = userDetailService.selectUserDetailByUserName(username);
+        model.addAttribute("userDetail", userDetail);
+        model.addAttribute("commons", Commons.getInstance());
+
 
 
         return "back/newback/article/statistic_charts";
---
+++ src/main/java/com/boot/pojo/password.java
@@ -0,0 +1,50 @@
+package com.boot.pojo;
+
+import io.swagger.annotations.Api;
+
+import java.io.Serializable;
+
+@Api("封装修改密码的信息")
+public class password implements Serializable {
+
+
+    private String oldPassword; //旧密码
+
+    private String newPassword; //新密码
+
+    private String checkPassword; //确认密码
+
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getCheckPassword() {
+        return checkPassword;
+    }
+
+    public void setCheckPassword(String checkPassword) {
+        this.checkPassword = checkPassword;
+    }
+
+    @Override
+    public String toString() {
+        return "password{" +
+                "oldPassword='" + oldPassword + '\'' +
+                ", newPassword='" + newPassword + '\'' +
+                ", checkPassword='" + checkPassword + '\'' +
+                '}';
+    }
+}
