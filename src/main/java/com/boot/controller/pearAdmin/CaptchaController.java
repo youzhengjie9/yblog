b77d7189770ab6143d38ad474654afdd044ba83b
ms666 <1550324080@qq.com>
2021-07-02 23:14:31 +0800

    2021/7/2 引入验证码

--- src/main/java/com/boot/controller/pearAdmin/CaptchaController.java
+++ src/main/java/com/boot/controller/pearAdmin/CaptchaController.java
@@ -1,5 +1,6 @@
 package com.boot.controller.pearAdmin;
 
+import com.github.pagehelper.util.MSUtils;
 import com.wf.captcha.ArithmeticCaptcha;
 import com.wf.captcha.utils.CaptchaUtil;
 import org.springframework.stereotype.Controller;
@@ -12,20 +13,45 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping(path = "/pear")
 public class CaptchaController {
 
+
+    /**
+     * public static void out(Captcha captcha, HttpServletRequest request, HttpServletResponse response)
+     * throws IOException {
+     * setHeader(response);
+     * request.getSession().setAttribute(SESSION_KEY, captcha.text().toLowerCase());
+     * captcha.out(response.getOutputStream());
+     * }
+     */
+
     @RequestMapping("/captcha")
     public void captcha(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-        // 设置位数
-        CaptchaUtil.out(5, request, response);
-        // 设置宽、高、位数
-        CaptchaUtil.out(130, 48, 5, request, response);
+        // 设置请求头为输出图片类型
+        response.setContentType("image/gif");
+        response.setHeader("Pragma", "No-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
 
-        // 使用算数验证码
-        ArithmeticCaptcha  captcha = new ArithmeticCaptcha(130,48,4);
-        captcha.setLen(3);  // 几位数运算，默认是两位
+        //字母类型
+        //SpecCaptcha captcha = new SpecCaptcha(130, 48, 4);
+
+        // 中文gif类型
+        //ChineseGifCaptcha captcha = new ChineseGifCaptcha(130, 48);
+
+        // 中文类型
+        //ChineseCaptcha captcha = new ChineseCaptcha(130, 48);
+
+        // 算术类型
+        ArithmeticCaptcha captcha = new ArithmeticCaptcha(130, 48); // 图片的宽高
+        captcha.setLen(2);  // 几位数运算，默认是两位
         captcha.getArithmeticString();  // 获取运算的公式：3+2=?
         captcha.text();  // 获取运算的结果：5
 
-        CaptchaUtil.out(request, response); //输出验证码
+        //验证码存入session或存入redis
+        //存redis通过UUID.randomUUID()生成key，specCaptcha.text().toLowerCase()生成value，存入redis并将key返回给前端，在登录时从redis中获取并验证
+        //存session则如下，
+        request.getSession().setAttribute("captcha", captcha.text().toLowerCase());
+        // 输出图片流
+        captcha.out(response.getOutputStream());
     }
 }
--- src/main/java/com/boot/controller/pearAdmin/catchDataController.java
+++ src/main/java/com/boot/controller/pearAdmin/catchDataController.java
@@ -1,28 +1,85 @@
 package com.boot.controller.pearAdmin;
 
+import com.alibaba.fastjson.JSON;
 import com.boot.annotation.Visitor;
+import com.boot.constant.Constant;
+import com.boot.data.ResponseData.ResponseJSON;
+import com.boot.data.ResponseData.layuiJSON;
+import com.boot.service.catchDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * @author 游政杰
  * @Date 2021/6/30
  */
 @Controller("pearCatchDataController")
 @RequestMapping(path = "/pear")
-@CrossOrigin
 public class catchDataController {
 
-
-
+    @Autowired
+    private catchDataService catchDataService;
 
     //爬取数据
     @Visitor(desc = "爬取数据")
     @RequestMapping(path = "/toCatchData")
     public String toCatchData() {
 
-
         return "back/newback/article/catch_list";
     }
+
+    /**
+     * 抓取文章接口
+     */
+    @RequestMapping(path = "/catch/Article")
+    @ResponseBody
+    public String catchArticle(String url,String code, Model model) {
+
+        layuiJSON json = new layuiJSON();
+
+
+
+        try {
+            catchDataService.catchData_csdn(url);
+
+            json.setMsg("抓取CSDN文章数据成功");
+            json.setSuccess(true);
+            return JSON.toJSONString(json);
+        } catch (Exception e) {
+            e.printStackTrace();
+            json.setMsg("抓取CSDN文章数据失败");
+            json.setSuccess(false);
+            return JSON.toJSONString(json);
+        }
+
+    }
+
+    /**
+     * 抓取模块接口
+     */
+    @RequestMapping(path = "/catch/ModelArticle")
+    @ResponseBody
+    public String catchModelArticle(String url,String code, Model model) {
+
+        layuiJSON json = new layuiJSON();
+
+        try {
+            catchDataService.batchCatchArticleByModel_csdn(url);
+            json.setMsg("抓取CSDN模块数据成功");
+            json.setSuccess(true);
+            return JSON.toJSONString(json);
+        } catch (Exception e) {
+            e.printStackTrace();
+            json.setMsg("抓取CSDN模块数据失败");
+            json.setSuccess(false);
+            return JSON.toJSONString(json);
+        }
+
+    }
+
 
 }
