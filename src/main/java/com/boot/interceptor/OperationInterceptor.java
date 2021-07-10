6cf7fdb01fb11fe387e1cab7e593227936f12967
ms666 <1550324080@qq.com>
2021-07-10 14:19:15 +0800

    2021/7/10 使用自定义注解+拦截器实现操作日志的收集

--- src/main/java/com/boot/interceptor/OperationInterceptor.java
+++ src/main/java/com/boot/interceptor/OperationInterceptor.java
@@ -1,12 +1,20 @@
 package com.boot.interceptor;
 
+import com.boot.annotation.Operation;
+import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
+/**
+ * @author 游政杰
+ */
 @Component
 public class OperationInterceptor implements HandlerInterceptor {
 
@@ -15,7 +23,21 @@ public class OperationInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestURI = request.getRequestURI();
         System.out.println(requestURI);
+        HandlerMethod handlerMethod= (HandlerMethod)handler;
+        String name = handlerMethod.getMethod().getName(); //获取方法名
+        Class<?> aClass = handlerMethod.getBean().getClass(); //获取类
+
+        MethodParameter[] methodParameters = handlerMethod.getMethodParameters();
+
+        Class<?>[] parameterType = new Class<?>[methodParameters.length]; //封装参数列表的所有类型
+        for (int i = 0; i < methodParameters.length; i++) {
+            parameterType[i]=methodParameters[i].getParameterType();
+        }
+
         //获取注解
+
+        Method method = aClass.getMethod(name, parameterType);
+        String value = method.getAnnotation(Operation.class).value(); //获取注解值
 
 
 
