6a763370bce28e81d76ef5cee0210cae624be506
ms666 <1550324080@qq.com>
2021-07-21 00:09:09 +0800

    2021/7/16 解决yblog在分布式的环境下SSO(单点登录)问题

--- src/main/java/com/boot/DemoApplication.java
+++ src/main/java/com/boot/DemoApplication.java
@@ -4,6 +4,7 @@ import com.boot.config.scanClassProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.TimeZone;
@@ -11,6 +12,7 @@ import java.util.TimeZone;
 @SpringBootApplication
 @EnableSwagger2 //开启swagger2
 @EnableConfigurationProperties(value = {scanClassProperties.class,}) //开启自定义配置类
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60*60*3) //开启springSession+redis分布式session
 public class DemoApplication {
 
     public static void main(String[] args) {
